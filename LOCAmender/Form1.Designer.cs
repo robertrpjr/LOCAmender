4107f20e3aedf7089b53dafa61c33b8a831fdfc5
Rob Perry <rperry@TCSDOMAIN.local>
2018-03-09 12:09:59 -0600

    2nd commit

---
+++ LOCAmender/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace LOCAmender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFolder = new System.Windows.Forms.Button();
+            this.amendFiles = new System.Windows.Forms.Button();
+            this.textBoxFolderSelected = new System.Windows.Forms.Label();
+            this.completeMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selectFolder
+            // 
+            this.selectFolder.Location = new System.Drawing.Point(206, 12);
+            this.selectFolder.Name = "selectFolder";
+            this.selectFolder.Size = new System.Drawing.Size(117, 23);
+            this.selectFolder.TabIndex = 0;
+            this.selectFolder.Text = "Select Folder";
+            this.selectFolder.UseVisualStyleBackColor = true;
+            this.selectFolder.Click += new System.EventHandler(this.selectFolder_Click);
+            // 
+            // amendFiles
+            // 
+            this.amendFiles.Location = new System.Drawing.Point(206, 114);
+            this.amendFiles.Name = "amendFiles";
+            this.amendFiles.Size = new System.Drawing.Size(113, 23);
+            this.amendFiles.TabIndex = 2;
+            this.amendFiles.Text = "Amend Files";
+            this.amendFiles.UseVisualStyleBackColor = true;
+            this.amendFiles.Click += new System.EventHandler(this.amendFiles_Click);
+            // 
+            // textBoxFolderSelected
+            // 
+            this.textBoxFolderSelected.Location = new System.Drawing.Point(78, 56);
+            this.textBoxFolderSelected.Name = "textBoxFolderSelected";
+            this.textBoxFolderSelected.Size = new System.Drawing.Size(373, 23);
+            this.textBoxFolderSelected.TabIndex = 4;
+            this.textBoxFolderSelected.Text = "Select a Folder";
+            this.textBoxFolderSelected.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // completeMessage
+            // 
+            this.completeMessage.Location = new System.Drawing.Point(140, 203);
+            this.completeMessage.Name = "completeMessage";
+            this.completeMessage.Size = new System.Drawing.Size(243, 23);
+            this.completeMessage.TabIndex = 5;
+            this.completeMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(541, 412);
+            this.Controls.Add(this.completeMessage);
+            this.Controls.Add(this.textBoxFolderSelected);
+            this.Controls.Add(this.amendFiles);
+            this.Controls.Add(this.selectFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Letter of Credit Amender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFolder;
+        private System.Windows.Forms.Button amendFiles;
+        private System.Windows.Forms.Label textBoxFolderSelected;
+        private System.Windows.Forms.Label completeMessage;
+    }
+}
+
---
+++ LOCAmender/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace LOCAmender
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+            amendFiles.Enabled = false; //prevent usage before selecting folder
+		}
+        
+		private void selectFolder_Click(object sender, EventArgs e)
+		{   //sets up folder picker
+			using(var fbd = new FolderBrowserDialog())
+			{
+				DialogResult result = fbd.ShowDialog();
+
+				if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+				{   
+					string[] allFiles = Directory.GetFiles(fbd.SelectedPath);
+					//puts folder path to string and displays
+                    textBoxFolderSelected.Text = fbd.SelectedPath;
+                    amendFiles.Enabled = true;
+
+				}
+
+		    }
+		}
+
+        private void amendFiles_Click(object sender, EventArgs e)
+        {
+            
+            //create array List
+            
+            string target_folder = textBoxFolderSelected.Text;
+            //placeholder for filenames
+            string fileNames = null;
+           
+            
+
+            var files = Directory.GetFiles(target_folder);
+            foreach (string file in files)
+            {
+                List<string> letters = new List<string>();
+                fileNames = System.IO.Path.GetFullPath(file);
+                string text = File.ReadAllText(file);
+                //intiate the printable string
+                string editedText = null;
+
+                letters.AddRange(Regex.Split(text, "\r\n---------------------------------------------------------------------\r\n").ToList());
+                
+                foreach (string item in letters)
+                {
+                    string amendedItem = item;
+
+                    
+                    string pattern1 = @"No.: \d{3,4}-\d{9,10}\s"; //regex for 1st Letter of credit # change 
+                    string pattern2 = @"\d{3,4}-\d{9,10}"; //regex for 2nd LOC # change
+                    
+                    //edits
+                    amendedItem = amendedItem.Replace("LETTER OF CREDIT", "AMENDED AND RESTATED LETTER OF CREDIT");
+                    amendedItem = amendedItem.Replace("Date of Issuance", "Effective Date");
+                    
+                    //sets pattern2 to a var to be called in following edits
+                    Match n = Regex.Match(amendedItem, pattern2);
+                    amendedItem = amendedItem.Replace("We h" ,"The previously issued Letter of Credit " + n.Value + " is hereby revoked. We h");
+                    amendedItem = amendedItem.Replace("Letter of Credit No. " + n.Value, "Amended and Restated Letter of Credit No. " + n.Value + "a");
+                    amendedItem = amendedItem.Replace(n.Value + " issued", n.Value + "a issued");
+
+                    foreach (Match m in Regex.Matches(amendedItem, pattern1))
+                    {
+                        amendedItem = amendedItem.Replace(m.Value, m.Value + "a");
+
+                    }
+                    //add to string 
+                    editedText += amendedItem + "\r\n---------------------------------------------------------------------\r\n";
+                }
+                
+
+                string fileOut = fileNames.Substring(0, fileNames.Length - 4) + "a.txt";
+                File.WriteAllText(fileOut, editedText);
+                
+                
+            }
+            
+            completeMessage.Text = "Complete";
+            amendFiles.Enabled = false; //prevent double clicking
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+	}
+}
+
